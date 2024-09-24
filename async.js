5f35674c4c85763025a6e637e7b1b1b25732ceaf
techbire <anshtechnical@gmail.com>
2024-09-24 23:13:15 +0530

    added

---
+++ async.js
@@ -0,0 +1,91 @@
+//normal
+// function AsyncFunction() {
+//     let p = new Promise(function(resolve) {
+//       // do some async logic here
+//       resolve("hi there!")
+//     });
+//     return p;
+//   }
+  
+//   function main() {
+//     AsyncFunction().then(function(value) {
+//         console.log(value);
+//     });
+//   }
+  
+//   main();
+
+
+//-------------------------------------------------------------------
+
+
+//async/await
+// function AsyncFunction() {
+//     let p = new Promise(function(resolve) {
+//       // do some async logic here
+//       resolve("hi there!")
+//     });
+//     return p;
+//   }
+  
+//   async function main() {
+//     const value = await AsyncFunction();
+//     console.log(value);
+//   }
+  
+//   main();
+
+//-------------------------------------------------------------------
+
+
+//callback
+// function AsyncFunction(callback) {
+//     // do some async logic here
+//     callback("hi there!")
+// }
+
+// async function main() {
+//   AsyncFunction(function(value) {
+//     console.log(value);
+//   });
+// }
+
+// main();
+
+//-------------------------------------------------------------------
+
+//promise(then)
+// function AsyncFunction() {
+//     let p = new Promise(function(resolve) {
+//       // do some async logic here
+//       resolve("hi there!")
+//     });
+//     return p;
+//   }
+  
+//   function main() {
+//     AsyncFunction().then(function(value) {
+//         console.log(value);
+//     });
+//   }
+  
+//   main();
+
+
+//-------------------------------------------------------------------
+
+//async/await
+// function AsyncFunction() {
+//     let p = new Promise(function(resolve) {
+//       // do some async logic here
+//       resolve("hi there!")
+//     });
+//     return p;
+//   }
+  
+//   async function main() {
+//     const value = await AsyncFunction();
+//     console.log(value);
+//   }
+  
+//   main();
